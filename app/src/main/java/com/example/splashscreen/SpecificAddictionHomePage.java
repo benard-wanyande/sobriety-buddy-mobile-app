6b24a94d42b84b7e1bb81877897cd0fe5bf12205
Adala Wanyande <awanyande@gmail.com>
2022-11-08 01:41:34 +0300

    Add Home Screen Page and Tabs

---
+++ app/src/main/java/com/example/splashscreen/SpecificAddictionHomePage.java
@@ -0,0 +1,35 @@
+package com.example.splashscreen;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+
+import com.google.android.material.tabs.TabLayout;
+
+public class SpecificAddictionHomePage extends AppCompatActivity {
+
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_specific_addiction_home_page);
+
+        tabLayout = findViewById(R.id.tablayout);
+        viewPager = findViewById(R.id.viewpager);
+
+        tabLayout.setupWithViewPager(viewPager);
+
+        VPAdapter vpAdapter = new VPAdapter(getSupportFragmentManager(), FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+        vpAdapter.addFragment(new SummaryFragment(), "Summary");
+        vpAdapter.addFragment(new CalendarFragment(), "Calendar");
+        vpAdapter.addFragment(new SettingsFragment(), "Settings");
+        viewPager.setAdapter(vpAdapter);
+    }
+
+
+
+
+}
